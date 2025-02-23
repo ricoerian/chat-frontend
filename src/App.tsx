e20e7828d8c36a545aff99d3341f4787883dc421
ricoerian <richoeriansyahm@gmail.com>
2025-02-23 18:50:46 +0700

    delete ThemeToggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
-import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors">
-      <ThemeToggle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/chat" element={<Chat />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
