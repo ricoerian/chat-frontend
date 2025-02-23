326ddd52a057dcbf203f1561075b6751147bcd76
ricoerian <richoeriansyahm@gmail.com>
2025-02-23 18:41:32 +0700

    pussss

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
+import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors">
+      <ThemeToggle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ToastProvider } from './context/ToastContext.tsx'
-import ThemeToggle from './components/ThemeToggle.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ToastProvider>
-      <ThemeToggle />
       <App />
     </ToastProvider>
   </StrictMode>,
